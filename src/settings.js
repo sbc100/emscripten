916703ee22e9bc4e2ceb69054c059339b046c305
Sam Clegg <sbc@chromium.org>
2023-01-15 07:22:08 -0800

    Limit the use of EXPORT_NAME to when MODULARIZE is set
    EXPORT_NAME doesn't make any sense without MODULARIZE.

--- src/settings.js
+++ src/settings.js
@@ -1183,7 +1183,6 @@ var DETERMINISTIC = false;
 // `EXPORT_NAME` option. We recommend renaming it to a more typical name for a
 // factory function, e.g. `createModule`.
 //
-//
 // You use the factory function like so:
 //
 //   const module = await EXPORT_NAME();
@@ -1250,6 +1249,7 @@ var BENCHMARK = false;
 
 // Global variable to export the module as for environments without a
 // standardized module loading system (e.g. the browser and SM shell).
+// Only makes sense when MODULARIZE is also specified.
 // [link]
 var EXPORT_NAME = 'Module';
 
